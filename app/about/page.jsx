44cdae618975a680f60996b0738593beec6289d7
cipher-ridge <jamesnjagi730@gmail.com>
2026-03-31 14:18:37 +0300

    UI technicalities addition

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,12 +1,14 @@
 import AboutHeroSection from '../components/AboutHeroSection';
 import AboutComp from '../components/AboutComp'
 import PropertyOwner from '../components/PropertySection'
+import ContactSection from '../components/ContactSection'
 export default function About(){
    return(
       <>
       <AboutHeroSection/>
       <AboutComp/>
       <PropertyOwner/>
+      <ContactSection/>
       </>
    )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,16 @@
 import Hero from './components/heroSection'
+import BrowseCategories from './components/BrowseCategorySection'
+import HomeSeekers from './components/HomeSeekerSection'
+import Testimonials from './components/TestimonialSection'
+import CTABanner from './components/CTAbannerSection'
 export default function Home() {
   return (
+    <>
     <Hero/>
+    <BrowseCategories/>
+    <HomeSeekers/>
+    <Testimonials/>
+    <CTABanner/>
+    </>
   );
 }
